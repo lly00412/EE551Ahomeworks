e180989a668eaefce9871d188e1f2ad87307ff9f
lly00412 <lchen39@stevens.edu>
2018-09-24 15:22:05 -0400

    Update setup.py

--- homework1/setup.py
+++ homework1/setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages
 setup(name='Homework1',
       version='1.0',
       description='Python Distribution Utilities',
-      author='Liyan Chen',
+      author='Sergul Aydore',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
